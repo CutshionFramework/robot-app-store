3c138c477a9ff7a151921ab33db745735a6776d6
junhui324 <junhuijang00@gmail.com>
2024-12-09 16:14:53 +0900

    Feat: open api 테스트

--- src/components/AppList.tsx
+++ src/components/AppList.tsx
@@ -11,22 +11,30 @@ import {
 
 import GetAppIcon from '@mui/icons-material/GetAppRounded';
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-console.log(backendUrl);
-
-async function handleButtonClick() {
-  try {
-    const response = await axios.post(
-      `${backendUrl}/api/download`,
-      { image_name: 'sromerof202/palletizing_app-server:latest' } // 데이터
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default function AppList() {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  console.log(backendUrl);
+
+  async function handleButtonClick(app_state: string) {
+    try {
+      if (app_state === 'Get') {
+        const response = await axios.post(
+          `${backendUrl}/api/download`,
+          { image_name: 'sromerof202/palletizing_app-server:latest' } // 데이터
+        );
+        console.log(response.data);
+      } else if (app_state === 'Run') {
+        const response = await axios.post(
+          `${backendUrl}/api/open`,
+          { image_name: 'sromerof202/palletizing_app-server:latest' } // 데이터
+        );
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <List>
@@ -34,7 +42,8 @@ export default function AppList() {
           <ListItem
             className={styles.list_item}
             key={app.id}
-            alignItems="flex-start">
+            alignItems="flex-start"
+          >
             <ListItemIcon>
               <img
                 className={styles.icon_image}
@@ -50,42 +59,13 @@ export default function AppList() {
               style={{ color: 'black' }}
               endIcon={<GetAppIcon />}
               className={styles.app_list_button}
-              onClick={handleButtonClick}>
-              Get
+              onClick={() => handleButtonClick(app.app_state)}
+            >
+              {app.app_state}
             </Button>
           </ListItem>
         ))}
       </List>
     </>
   );
-}
-
-{
-  /* <div className={styles.AppList}>
-        <h2>Pionoid App List</h2>
-        <ul>
-          {AppData.map((app) => (
-            <>
-              <li key={app.id}>
-                <img
-                  className={styles.icon_image}
-                  src={`/src/assets/icons/${app.id}.jpg`}
-                  alt="app_icon"
-                />
-                <div className={styles.app_list_content}>
-                  <strong>{app.name}</strong>
-                  <p>{app.description}</p>
-                </div>
-                <div className={styles.app_list_button}>
-                  <Button
-                    style={{ color: 'black' }}
-                    endIcon={<GetAppIcon />}>
-                    Get
-                  </Button>
-                </div>
-              </li>
-            </>
-          ))}
-        </ul>
-      </div> */
 }
